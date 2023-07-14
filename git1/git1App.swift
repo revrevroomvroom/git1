ccbb98508aa2aab4b80fd8ca7b11e6554f1dd7a5
rev <revatikaluskar@icloud.com>
2023-07-14 15:16:37 +0100

    Initial Commit

---
+++ git1/git1App.swift
@@ -0,0 +1,17 @@
+//
+//  git1App.swift
+//  git1
+//
+//  Created by Revati Kaluskar on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct git1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
